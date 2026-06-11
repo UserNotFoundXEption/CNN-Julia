7f014d96bd754441c4274171245236d22ea0e89b
Kamil <01178868@pw.edu.pl>
2026-06-11 23:27:20 +0200

    Hopefully final version

--- src/layer_conv.jl
+++ src/layer_conv.jl
@@ -5,20 +5,16 @@ struct Conv
     pad::Int
 end
 
-function Conv(kh::Int, kw::Int, cin::Int, cout::Int; pad::Int=0)
+function Conv(kh::Int, kw::Int, cin::Int, cout::Int; pad::Int = 0)
     W = Node(he_init_conv(kh, kw, cin, cout))
     return Conv(W, pad)
 end
 
-function pad_input(x::Array{Float32,4}, pad::Int)
-    if pad == 0
-        return x
-    end
-
+function pad_input(x::Array{Float32,4}, pad::Int)::Array{Float32,4}
+    pad == 0 && return x
     h, w, c, b = size(x)
     xp = zeros(Float32, h + 2pad, w + 2pad, c, b)
-
-    @views @inbounds xp[pad+1:pad+h, pad+1:pad+w, :, :] .= x
+    @inbounds @views xp[pad+1:pad+h, pad+1:pad+w, :, :] .= x
     return xp
 end
 
@@ -27,30 +23,34 @@ function crop_unpad!(dx::Array{Float32,4}, dxp::Array{Float32,4}, pad::Int)
         dx .+= dxp
     else
         h, w, c, b = size(dx)
-        @views @inbounds dx .+= dxp[pad+1:pad+h, pad+1:pad+w, :, :]
+        @inbounds @views dx .+= dxp[pad+1:pad+h, pad+1:pad+w, :, :]
     end
 end
 
-function im2col(xp::Array{Float32,4}, kh::Int, kw::Int)
+# tensor -> kolumna
+function im2col(
+    xp::Array{Float32,4},
+    kh::Int,
+    kw::Int,
+)::Tuple{Matrix{Float32},Int,Int}
     hp, wp, cin, bsz = size(xp)
-
-    oh = hp - kh + 1
-    ow = wp - kw + 1
-
+    oh  = hp - kh + 1
+    ow  = wp - kw + 1
     ksize = kh * kw * cin
     ncols = oh * ow * bsz
 
     cols = Matrix{Float32}(undef, ksize, ncols)
 
     @inbounds for n in 1:bsz
+        nbase = (n - 1) * oh * ow
         for i in 1:oh
+            ibase = nbase + (i - 1) * ow
             for j in 1:ow
-                col = (n - 1) * oh * ow + (i - 1) * ow + j
+                col = ibase + j
                 row = 1
-
                 for ci in 1:cin
                     for u in 1:kh
-                        for v in 1:kw
+                        @simd for v in 1:kw
                             cols[row, col] = xp[i+u-1, j+v-1, ci, n]
                             row += 1
                         end
@@ -63,25 +63,27 @@ function im2col(xp::Array{Float32,4}, kh::Int, kw::Int)
     return cols, oh, ow
 end
 
+# odwrotnosc im2col do liczenia gradientow
 function col2im!(
     dxp::Array{Float32,4},
     dcols::Matrix{Float32},
     kh::Int,
     kw::Int,
     oh::Int,
-    ow::Int
+    ow::Int,
 )
     hp, wp, cin, bsz = size(dxp)
 
     @inbounds for n in 1:bsz
+        nbase = (n - 1) * oh * ow
         for i in 1:oh
+            ibase = nbase + (i - 1) * ow
             for j in 1:ow
-                col = (n - 1) * oh * ow + (i - 1) * ow + j
+                col = ibase + j
                 row = 1
-
                 for ci in 1:cin
                     for u in 1:kh
-                        for v in 1:kw
+                        @simd for v in 1:kw
                             dxp[i+u-1, j+v-1, ci, n] += dcols[row, col]
                             row += 1
                         end
@@ -92,20 +94,23 @@ function col2im!(
     end
 end
 
+# wynik mnozenia macierzy -> tensor 4d
 function mat_to_4d!(
     out_val::Array{Float32,4},
     out_mat::Matrix{Float32},
     oh::Int,
     ow::Int,
     cout::Int,
-    bsz::Int
+    bsz::Int,
 )
     @inbounds for n in 1:bsz
+        nbase = (n - 1) * oh * ow
         for i in 1:oh
+            ibase = nbase + (i - 1) * ow
             for j in 1:ow
-                col = (n - 1) * oh * ow + (i - 1) * ow + j
-
-                for co in 1:cout
+                col = ibase + j
+                # co is the first (fastest-varying) dimension of out_val
+                @simd for co in 1:cout
                     out_val[i, j, co, n] = out_mat[co, col]
                 end
             end
@@ -113,21 +118,23 @@ function mat_to_4d!(
     end
 end
 
+# gradient wyyjscia z tensora -> macierz
 function grad_to_mat(
     grad::Array{Float32,4},
     oh::Int,
     ow::Int,
     cout::Int,
-    bsz::Int
-)
+    bsz::Int,
+)::Matrix{Float32}
     gmat = Matrix{Float32}(undef, cout, oh * ow * bsz)
 
     @inbounds for n in 1:bsz
+        nbase = (n - 1) * oh * ow
         for i in 1:oh
+            ibase = nbase + (i - 1) * ow
             for j in 1:ow
-                col = (n - 1) * oh * ow + (i - 1) * ow + j
-
-                for co in 1:cout
+                col = ibase + j
+                @simd for co in 1:cout
                     gmat[co, col] = grad[i, j, co, n]
                 end
             end
@@ -137,13 +144,15 @@ function grad_to_mat(
     return gmat
 end
 
+
+
 function forward(l::Conv, x::Node{Array{Float32,4}})
-    xval = x.value
-    wval = l.W.value
-    pad = l.pad
+    xval = x.value          ::Array{Float32,4}
+    wval = l.W.value        ::Array{Float32,4}
+    pad  = l.pad            ::Int
 
     kh, kw, cin, cout = size(wval)
-    h, w, c, bsz = size(xval)
+    h,  w,  c,  bsz   = size(xval)
 
     @assert c == cin "Conv channel mismatch: input has $c, weights expect $cin"
 
@@ -151,12 +160,10 @@ function forward(l::Conv, x::Node{Array{Float32,4}})
 
     xcol, oh, ow = im2col(xp, kh, kw)
 
-    # W: kh × kw × cin × cout
     # Wmat: cout × (kh*kw*cin)
     Wmat = reshape(wval, kh * kw * cin, cout)'
 
-    # GEMM:
-    # out_mat: cout × (oh*ow*bsz)
+    # GEMM: cout × (oh*ow*bsz)
     out_mat = Wmat * xcol
 
     out_val = Array{Float32,4}(undef, oh, ow, cout, bsz)
@@ -166,21 +173,16 @@ function forward(l::Conv, x::Node{Array{Float32,4}})
     out.parents = [x, l.W]
 
     out.backward_fn = () -> begin
-        gmat = grad_to_mat(out.grad, oh, ow, cout, bsz)
+        gmat  = grad_to_mat(out.grad, oh, ow, cout, bsz)
 
-        # dWmat: cout × ksize
-        dWmat = gmat * xcol'
-
-        # dXcol: ksize × ncols
-        dXcol = Wmat' * gmat
+        dWmat = gmat * xcol'          # cout × ksize
+        dXcol = Wmat' * gmat          # ksize × ncols
 
         dxp = zeros(Float32, size(xp))
         col2im!(dxp, dXcol, kh, kw, oh, ow)
 
         crop_unpad!(x.grad, dxp, pad)
 
-        # dWmat' ma rozmiar ksize × cout,
-        # więc można go przekształcić z powrotem do kh × kw × cin × cout
         l.W.grad .+= reshape(dWmat', kh, kw, cin, cout)
     end
 
--- src/layer_maxpool.jl
+++ src/layer_maxpool.jl
@@ -7,34 +7,35 @@ end
 MaxPool(kh::Int, kw::Int; stride::Int=kh) = MaxPool(kh, kw, stride)
 
 function forward(l::MaxPool, x::Node{Array{Float32,4}})
-    xval = x.value
-    @assert ndims(xval) == 4 "MaxPool expects H×W×C×B input"
+    xval = x.value          ::Array{Float32,4}
+    @assert ndims(xval) == 4 "MaxPool expects HxWxCxB input"
 
-    h, w, c, bsz = size(xval)
+    h, w, c, bsz  = size(xval)
     kh, kw, stride = l.kh, l.kw, l.stride
 
     oh = fld(h - kh, stride) + 1
     ow = fld(w - kw, stride) + 1
 
-    out_val = zeros(Float32, oh, ow, c, bsz)
-    maxpos = Array{NTuple{2,Int}}(undef, oh, ow, c, bsz)
+    out_val  = Array{Float32,4}(undef, oh, ow, c, bsz)
+    maxpos_u = Array{Int,4}(undef, oh, ow, c, bsz)
+    maxpos_v = Array{Int,4}(undef, oh, ow, c, bsz)
 
     @inbounds for n in 1:bsz
         for ch in 1:c
             for i in 1:oh
+                hs = (i - 1) * stride + 1
                 for j in 1:ow
-                    hs = (i - 1) * stride + 1
                     ws = (j - 1) * stride + 1
 
-                    best = -Inf32
+                    best  = -Inf32
                     bestu = hs
                     bestv = ws
 
                     for u in 0:kh-1
-                        for v in 0:kw-1
+                        @simd for v in 0:kw-1
                             val = xval[hs+u, ws+v, ch, n]
                             if val > best
-                                best = val
+                                best  = val
                                 bestu = hs + u
                                 bestv = ws + v
                             end
@@ -42,7 +43,8 @@ function forward(l::MaxPool, x::Node{Array{Float32,4}})
                     end
 
                     out_val[i, j, ch, n] = best
-                    maxpos[i, j, ch, n] = (bestu, bestv)
+                    maxpos_u[i, j, ch, n] = bestu
+                    maxpos_v[i, j, ch, n] = bestv
                 end
             end
         end
@@ -52,12 +54,14 @@ function forward(l::MaxPool, x::Node{Array{Float32,4}})
     out.parents = [x]
 
     out.backward_fn = () -> begin
+        xgrad  = x.grad     ::Array{Float32,4}
+        ograd  = out.grad   ::Array{Float32,4}
+
         @inbounds for n in 1:bsz
             for ch in 1:c
                 for i in 1:oh
-                    for j in 1:ow
-                        u, v = maxpos[i, j, ch, n]
-                        x.grad[u, v, ch, n] += out.grad[i, j, ch, n]
+                    @simd for j in 1:ow
+                        xgrad[maxpos_u[i,j,ch,n], maxpos_v[i,j,ch,n], ch, n] += ograd[i, j, ch, n]
                     end
                 end
             end
